0ee119bc14bd9bc88b669f75672102ddb1211819
Fatima Ajroush <ajfatima41@gmail.com>
2024-07-23 06:13:12 +0300

    Pushing project to github

---
+++ Bulky.DataAccess/Migrations/20231111200859_CorrectSpellingMistakeInOrderheader.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBook.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrectSpellingMistakeInOrderheader : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderHeaders_AspNetUsers_ApplicastionUserId",
+                table: "OrderHeaders");
+
+            migrationBuilder.RenameColumn(
+                name: "ApplicastionUserId",
+                table: "OrderHeaders",
+                newName: "ApplicationUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderHeaders_ApplicastionUserId",
+                table: "OrderHeaders",
+                newName: "IX_OrderHeaders_ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderHeaders_AspNetUsers_ApplicationUserId",
+                table: "OrderHeaders",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderHeaders_AspNetUsers_ApplicationUserId",
+                table: "OrderHeaders");
+
+            migrationBuilder.RenameColumn(
+                name: "ApplicationUserId",
+                table: "OrderHeaders",
+                newName: "ApplicastionUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderHeaders_ApplicationUserId",
+                table: "OrderHeaders",
+                newName: "IX_OrderHeaders_ApplicastionUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderHeaders_AspNetUsers_ApplicastionUserId",
+                table: "OrderHeaders",
+                column: "ApplicastionUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Bulky.DataAccess/Migrations/20231116105008_correctSpellingMistakesInOrderHeader.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBook.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class correctSpellingMistakesInOrderHeader : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TrackingNubmer",
+                table: "OrderHeaders",
+                newName: "TrackingNumber");
+
+            migrationBuilder.RenameColumn(
+                name: "ShipDate",
+                table: "OrderHeaders",
+                newName: "ShippingDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TrackingNumber",
+                table: "OrderHeaders",
+                newName: "TrackingNubmer");
+
+            migrationBuilder.RenameColumn(
+                name: "ShippingDate",
+                table: "OrderHeaders",
+                newName: "ShipDate");
+        }
+    }
+}
